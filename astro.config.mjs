b203f3fb4fea3a1f0b20c56f988cf5a2e5fefc32
Luis Velasco <luiselsw27@gmail.com>
2025-10-02 21:20:15 +0200

    Proyecto trendyup con todos los cambios realizados

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,24 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
+
   server: {
       allowedHosts: [
-        '.wklysocial.com',
-        '.wklysocial.com'
+        '.trendyup.es',
+        '.www.trendyup.es'
       ]
     },
+
   vite: {
     plugins: [tailwindcss()],
-  }
+  },
+
+  integrations: [react()]
 });
